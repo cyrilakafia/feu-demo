489d1979e3a672ecc75a5479674fab73a3ee7e54
Cyril Akafia <kwakucyril@gmail.com>
2024-02-06 11:20:20 -0500

    latest_changes

--- app.py
+++ app.py
@@ -2,28 +2,52 @@ import streamlit as st
 import os
 import torch
 from inf import run_inference
-from visualize_heatmap import visualize_heatmap
+from visualize_heatmap import viz_heatmap
 
+# Set page title and favicon.
 st.set_page_config(page_title="DPnSSM", page_icon="♓", layout="wide")
 
-col1, mid, col2 = st.columns([1,1,20])
+# Set up the title and logo
+col1, mid1, col2, mid2, col3 = st.columns([1,1,1,1,20])
 with col1:
     st.image('./images/AZALab_Logo_Social Profile Image_Mark Only.jpg', width=100)
+
 with col2:
+    st.image('./images/sparsitas.jpg', width=75)
+
+with col3:
     st.title("FEU - Functional Encoding Units")
+
+
+# Set up a test case
+if 'test' not in st.session_state:
+    st.session_state['test'] = False
 
 # Create two columns for the split view
 left_column, right_column = st.columns([2, 3])
 
 with left_column:
     with st.form("form"):
-        uploaded_files = st.file_uploader("Upload your spike data in pickle format with extension .p", accept_multiple_files=True)
+        uploaded_file = st.file_uploader("Upload your spike data in pickle format with extension .p", accept_multiple_files=False)
         iter = st.slider('Number of iterations', 1, 1500, 1)
         st.info('Larger numbers would increase accuracy but would significantly increase processing time', icon="ℹ️")
         submitted = st.form_submit_button("Run")
 
+    # Test run button
+    if st.button("Run test with default data", key="test_button"):
+        st.session_state['test'] = True
+        st.write("Running test...")
+        iter = 1  
+        submitted = True  
+
     if submitted:
-        for i, uploaded_file in enumerate(uploaded_files):
+        if st.session_state['test']:
+            # Handle test run
+            st.toast('Running test inference', icon='⏳')
+            output = run_inference('default/sim1231_true.p', title='1', device='cpu', iterations=iter, seed=None)
+            st.toast('Test Inference Completed!', icon='✅')
+
+        elif uploaded_file:
             if uploaded_file is not None:
                 bytes_data = uploaded_file.getvalue()
 
@@ -34,7 +58,7 @@ with left_column:
                     f.write(bytes_data)
 
                 key = uploaded_file.name
-                if st.button("❌ Cancel process", key=key):
+                if st.button("❌ Cancel", key=key):
                     st.warning('Process cancelled', icon='⚠️')
                     st.stop()
 
@@ -42,39 +66,50 @@ with left_column:
                 output = run_inference(os.path.join('uploads', uploaded_file.name), title='1', device='cpu', iterations=iter, seed=None)
                 st.toast('Inference Completed!', icon='✅')
 
-            with right_column:
-                if len(uploaded_files) == 1:
-                    visualize_heatmap('inference/pickle.p')
-                    st.image('images/heatmap.png')
-                    os.remove('images/heatmap.png')
+        else: 
+            st.warning('No file uploaded', icon='⚠️')
+            st.stop()
 
-                if len(uploaded_files) == 2 and i == 1:
-                    visualize_heatmap('inference/pickle.p')
-                    visualize_heatmap('inference/pickle_2.p')
+        with right_column:
+            if st.session_state['test'] and submitted:
+                best_assigns = viz_heatmap('1', iter = iter)
+                st.image('images/heatmap.png')
+                os.remove('images/heatmap.png')
 
-                    col1, col2 = st.columns(2)
+            elif uploaded_file:
+                best_assigns = viz_heatmap('1', iter = iter)
+                st.image('images/heatmap.png')
+                os.remove('images/heatmap.png')
 
-                    col1.write(uploaded_files[0].name)
-                    col1.image('images/heatmap.png')
-                    os.remove('images/heatmap.png')
+                # Remove uploaded file
+                os.remove(os.path.join('uploads', uploaded_file.name))
 
-                    col2.write(uploaded_files[1].name)
-                    col2.image('images/heatmap_2.png')
-                    os.remove('images/heatmap_2.png')
+            
+        @st.cache_data
+        def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+            return df.to_csv().encode('utf-8')
+        
+        csv = convert_df(best_assigns)
 
-            os.remove(os.path.join('uploads', uploaded_file.name))
+        st.write(best_assigns)
+        
+        col1, col2 = st.columns(2)
 
-            # Download buttons are now here, below the "Cancel process" button
+        with col1:
             st.download_button(
                 label="🗂️ Download cluster assignments",
-                data=f'outputs/sim1_assigns.csv',
-                file_name=f'sim1_assigns.csv',
+                data= csv,
+                file_name=f'best_assigns.csv',
                 mime="text/csv",
             )
 
+        with col2:
             st.download_button(
                 label="🗂️ Download cluster parameters",
                 data="outputs/sim1_params.tsv",
                 file_name="sim1_params.tsv",
                 mime="text/tab-separated-values",
             )
+    if st.session_state['test'] and submitted:
+        st.session_state['test'] = False
--- visualize_heatmap.py
+++ visualize_heatmap.py
@@ -1,14 +1,72 @@
 import matplotlib.pyplot as plt
-import matplotlib as mpl
+import pandas as pd
+import numpy as np
+import os
 
-from utils.data import read_pickle, load_network
-from utils.vis import reorder_cluster_ids, vis_heatmap
+def ids_to_coo(ids):
+    '''
+    Convert cluster assignments to a coo matrix
 
-def visualize_heatmap(filename):
-    out = read_pickle(filename)
+    Args:
+    ids (np.array): the cluster assignments
 
-    cluster_ids, cluster_params, n_clusters = reorder_cluster_ids(*out)
+    Returns:
+    coo (np.array): the coo matrix
+    '''
+    
+    coo = np.zeros((len(ids), len(ids)))
+    for u in np.unique(ids):
+        mask = ids == u
+        coo[np.ix_(mask, mask)] = 1
+    return coo
 
-    burnin = 1000
-    sim_mat, all_best_gibbs, all_sim_mats = vis_heatmap(cluster_ids)
-    return sim_mat, all_best_gibbs, all_sim_mats
+def viz_heatmap(run, iter):
+    '''
+    Visualize the cluster assignments as a heatmap
+
+    Args:
+    run (str): the run number
+    iter (int): the number of iterations
+
+    Returns:
+    best_assigns (pd.DataFrame): the best cluster assignments
+    
+    Saves a heatmap of the cluster assignments to images/heatmap.png
+    '''
+
+    if iter < 510:
+        assigns = pd.read_csv(f"outputs/sim{run}_assigns.csv", header=None)
+    else:
+        assigns = pd.read_csv(f"outputs/sim{run}_assigns.csv", header=None).iloc[500:].reset_index(drop=True)
+
+    coos = np.stack([ids_to_coo(x) for _, x in assigns.iterrows()])
+    mean_coo = coos.mean(axis=0)
+    dists = np.linalg.norm(coos - mean_coo, axis=(1, 2))
+    min_dist_idx = np.argmin(dists)
+    match_idxs = np.where(np.all(coos == coos[min_dist_idx], axis=(1, 2)))[0]
+
+    # save to file
+    best_assigns =  pd.DataFrame(assigns.loc[min_dist_idx])
+    best_assigns.columns = ['cluster id']
+
+    # rotate the df
+    best_assigns = best_assigns.T
+
+    reorder = assigns.loc[min_dist_idx].sort_values().index
+
+    # plot cluster assignments
+    plt.imshow(mean_coo[np.ix_(reorder, reorder)], cmap="coolwarm")
+    plt.colorbar()
+
+    N = len(assigns.columns)
+
+    plt.yticks(np.arange(N), reorder, fontsize=5)
+    plt.xticks(np.arange(N), reorder, fontsize=5, rotation=90)
+    plt.tight_layout()
+    if not os.path.exists('images/heatmap.png'):
+        plt.savefig('images/heatmap.png', dpi = 500)
+    else:
+        plt.savefig('images/heatmap_2.png', dpi = 500)
+
+    return best_assigns
+
